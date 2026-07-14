73cff859e2d1ea3f7afa37a216a5344bcea49d6e
Caxtone Technologies Kenya <tolecaxtone2@gmail.com>
2026-07-14 17:22:39 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+
+@app.route('/')
+def index():
+    """Serve the main page"""
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
